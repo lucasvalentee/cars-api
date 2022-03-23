6d1db552d28ef35fdecaff002aed487169a64537
Lucas Valente <lucas.valente@asksuite.com>
2022-03-22 22:04:39 -0300

    feat: Modified all files to practice SOLID concepts.

---
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -0,0 +1,43 @@
+import Specification from '../../model/Specification';
+import {
+  ISpecificationDTO,
+  ISpecificationsRepository,
+} from '../ISpecificationsRepository';
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  private static INSTANCE: SpecificationsRepository;
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  public static getInstance() {
+    if (!SpecificationsRepository.INSTANCE) {
+      SpecificationsRepository.INSTANCE = new SpecificationsRepository();
+    }
+
+    return SpecificationsRepository.INSTANCE;
+  }
+
+  create({ name, description }: ISpecificationDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      created_at: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+
+  findByName(name: string) {
+    return this.specifications.find(
+      specification => specification.name === name,
+    );
+  }
+}
+
+export default SpecificationsRepository;
