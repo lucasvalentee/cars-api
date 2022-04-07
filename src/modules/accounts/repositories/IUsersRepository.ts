b77a3353529fbdc4c3133c02441dff53f46ebb13
Lucas Valente <lucas.valente@asksuite.com>
2022-04-07 00:11:14 -0300

    refactor: Changed parameters of create method on user repo. interface.

--- src/modules/accounts/repositories/IUsersRepository.ts
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -2,7 +2,14 @@ import ICreateUserDTO from '../dtos/ICreateUserDTO';
 import User from '../entities/User';
 
 interface IUsersRepository {
-  create(data: ICreateUserDTO): Promise<void>;
+  create({
+    id,
+    name,
+    password,
+    email,
+    drive_license,
+    avatar,
+  }: ICreateUserDTO): Promise<void>;
   findByEmail(email: string): Promise<User>;
   findById(id: string): Promise<User>;
 }
